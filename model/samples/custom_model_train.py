8e23296d34f4edb4e1cb2a9ba34c160c4adcdb8c
merri4 <kjhwang97@gmail.com>
2024-03-14 03:22:21 +0900

    feat: custom model 간략화

--- model/samples/custom_model_train.py
+++ model/samples/custom_model_train.py
@@ -1,5 +1,11 @@
 # https://www.kaggle.com/code/angyalfold/hugging-face-bert-with-custom-classifier-pytorch#Tokenization
 
+### 1. custom model의 구조 완성하기 ㅇ
+### 2. 실제로 daic-woz를 sample | target | *features로 변환하기
+### 3. daic-woz를 벡터화하는 데이터로더 코드 만들기
+
+### Warning : bert에 88개 추가 feature 넣는 것에 맞춰져 있음. non-bert 모델로 변경 시, 512나 768로 하드코딩된 부분 수정할 것.
+
 import os
 import random
 from tqdm import tqdm
@@ -12,6 +18,7 @@ from torch import nn
 from torch.utils.data import Dataset
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorWithPadding, BertModel
+from transformers.modeling_outputs import SequenceClassifierOutput
 from transformers import TrainingArguments, Trainer
 
 import evaluate
@@ -30,14 +37,15 @@ def preprocess_function(examples):
 def parse_arguments() :
 
     parser = argparse.ArgumentParser(description='Argparse')
-    parser.add_argument('--data_dir', type=str, default="./data/kor_sample.csv")
+    parser.add_argument('--model_name', type=str, default="google-bert/bert-base-uncased")
+    parser.add_argument('--data_dir', type=str, default="./data/final_features.csv")
     parser.add_argument('--output_dir', type=str, default="./output_custom")
     parser.add_argument('--dev_ratio', type=float, default=0.2)
     parser.add_argument('--save_total_limit', type=int, default=5)
-    parser.add_argument('--eval_steps', type=int, default=250)
+    parser.add_argument('--eval_steps', type=int, default=12)
     parser.add_argument('--epoch', type=int, default=2)
     parser.add_argument('--lr', type=float, default=3e-5)
-    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--batch_size', type=int, default=16)
 
     args = parser.parse_args()
 
@@ -50,13 +58,14 @@ class DaicWozDataset(Dataset):
     def __init__(self, data_dir, tokenizer):
         
         self.data = pd.read_csv(data_dir)
+        self.addi_feat_df = self.data.drop(columns=["id", "text", "target"]) # 88개만 남김
         self.labels = self.data['target'].tolist()
         self.inputs = []
 
         for text in tqdm(self.data["text"]) :
             tmp = tokenizer(
                 text,
-                max_length=512,
+                max_length=512, # warning : non-bert시 그에 맞게 변경할 것
                 padding="max_length",
                 truncation=True,
                 # return_token_type_ids=True,
@@ -65,46 +74,70 @@ class DaicWozDataset(Dataset):
             self.inputs.append(tmp)
         
     def __getitem__(self, idx):
-
         return {
             'input_ids': self.inputs[idx]['input_ids'].squeeze(),
             'attention_mask': self.inputs[idx]['attention_mask'].squeeze(),
-            # 'token_type_ids': self.inputs[idx]['token_type_ids'].squeeze(),
-            'labels': self.labels[idx]
+            'addi_feat' : self.addi_feat_df.loc[idx].values,
+            'labels': self.labels[idx],
         }
     
     def __len__(self):
         return len(self.labels)
 
+
 class CustomModel(nn.Module) :
-    def __init__(self, model_name, linear_size, num_label, addi_feat) :
+    def __init__(self, model_name, linear_size, num_labels, addi_feat_size) :
         super(CustomModel, self).__init__()
+        self.num_labels = num_labels
         self.bert = BertModel.from_pretrained(model_name)
         
-        # nn.Linear로 처리해도 됨
-        self.dropout1 = nn.Dropout()
-        self.linear1 = nn.Linear(in_features= 768 + addi_feat, out_features=linear_size)
-        self.batch_norm1 = nn.BatchNorm1d(num_features=linear_size)
-        
-        self.dropout2 = nn.Dropout(p=0.8)
-        self.linear2 = nn.Linear(in_features=linear_size, out_features=num_label)
-        self.batch_norm2 = nn.BatchNorm1d(num_features=num_label)
-        
+        # todo : nn.Sequential로 처리?
+        # https://github.com/huggingface/transformers/blob/v4.38.2/src/transformers/models/bert/modeling_bert.py#L1512 참고
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc = nn.Linear((768 + addi_feat_size), linear_size) # warning : 모델이 변경될 경우 768이 아니라 모델 최종 output size로 직접 바꿔줘야 함
+        self.dropout2 = nn.Dropout(0.8)
+        self.classifier = nn.Linear(linear_size, num_labels)
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, input_ids, attention_mask):
+    def forward(self, input_ids, attention_mask, addi_feat, labels):
         
         bert_output = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        
+        # bert_output으로 2가지가 나옴 : 
+        # bert_output["last_hidden_state"] : torch.Size([1, 512, 768])
+        # bert_output["pooler_output"] : torch.Size([1, 768])
+        # 이 단계에서 bert_output[1]을 쓰는 건 pooler_output 을 사용하겠다는 뜻
         x = self.dropout1(bert_output[1])
+        # print("bert output shape : {}".format(x.shape))
 
-        # 이 단계에서 feeidng 추가
-        x = self.linear1(x)
+        # 이 단계에서 추가 feature concat.
+        x = self.concat_features(x, addi_feat)
+        # print("add feat shape : {}".format(addi_feat.shape))
+        # print("concat x shape : {}".format(x.shape))
+        x = self.fc(x)
         x = self.dropout2(x)
-        x = self.batch_norm1(x)
-        x = self.linear2(x)
-        x = self.batch_norm2(x)
-        return self.sigmoid(x)
+        x = self.classifier(x)
+        logits = self.sigmoid(x)
+
+        # print(logits)
+        # print(labels)
+
+        loss_fct = nn.CrossEntropyLoss()
+        loss = loss_fct(logits.view(-1, self.num_labels), torch.Tensor(labels).long().view(-1))
+
+        print(loss)
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=bert_output.hidden_states,
+            attentions=bert_output.attentions,
+        )
     
+    def concat_features(self, x, addi_feat) :
+        addi_feat = torch.Tensor(addi_feat)
+        return torch.cat((x, addi_feat), dim=1)
+
     def freeze_bert(self):
         for param in self.bert.named_parameters():
             param[1].requires_grad=False
@@ -130,21 +163,15 @@ if __name__ == "__main__" :
 
     # 모델 생성
 
-    model_name = "kykim/bert-kor-base"
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name)
+    # data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
-    id2label = {0: "NEGATIVE", 1: "POSITIVE"}
-    label2id = {"NEGATIVE": 0, "POSITIVE": 1}
-
-    model_custom = CustomModel(model_name, 256, 1).to(DEVICE)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2, id2label=id2label, label2id=label2id).to(DEVICE)
-    print(model)
-    print(model_custom)
+    model_custom = CustomModel(args.model_name, 256, 2, 88).to(DEVICE) # warning : 88개로 선언했으니 그만큼 들어가야 함
+    # print(model_custom)
     
     # 데이터셋 생성
     dataset = DaicWozDataset(args.data_dir, tokenizer)
-    dataset_train, dataset_dev = train_test_split(dataset, test_size=args.dev_ratio, random_state=SEED)
+    # dataset_train, dataset_dev = train_test_split(dataset, test_size=args.dev_ratio, random_state=SEED)
 
     ### =============================================================
     ### Train
@@ -183,14 +210,13 @@ if __name__ == "__main__" :
     )
 
     trainer = Trainer(
-        model=model,
+        model=model_custom,
         args=training_args,
-        train_dataset=dataset_train,
-        eval_dataset=dataset_dev,
+        train_dataset=dataset,
+        eval_dataset=dataset,
         tokenizer=tokenizer,
-        data_collator=data_collator,
+        # data_collator=data_collator,
         compute_metrics=compute_metrics,
     )
 
     trainer.train()
-    model.save_pretrained('./best_model')
